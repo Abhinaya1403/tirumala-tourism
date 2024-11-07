a854b9d724f73d622227a2e3aecd5ef8fdd63eb2
Abhinaya1403 <155753753+Abhinaya1403@users.noreply.github.com>
2024-11-07 22:58:48 +0530

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,56 @@
+function redirectTotirumala_tirupathi_devasthanamPage() {
+    // Display an alert
+    alert('Click on OK to see more about TIRUMALA TIRUPATHI DEVASTHANAM');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'tirumala_tirupathi_devasthanam.html';
+}
+function varaha_swamy() {
+    // Display an alert
+    alert('Click on OK to see more about Sri Varaha Swamy Temple');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'varaha_swamy.html';
+}
+function vengamamba() {
+    // Display an alert
+    alert('Click on OK to see more about Matrusri Tarigonda Vengamamba Temple');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Matrusri_Tarigonda_Vengamamba.html';
+}
+function raghavendra() {
+    // Display an alert
+    alert('Click on OK to see more about Sri Raghavendra Swamy Temple');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Sri_Raghavendra_swamy_Temple.html';
+}
+function japalianjaneya() {
+    // Display an alert
+    alert('Click on OK to see more about Japali Anjaneya Swami Devalayam');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Japali_Anjaneya_Swami_Devalayam.html';
+}
+function Akashaganga() {
+    // Display an alert
+    alert('Click on OK to see more about Akashaganga');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Akashaganga.html';
+}
+function Papavinasam() {
+    // Display an alert
+    alert('Click on OK to see more about Papavinasam');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Papavinasam.html';
+}
+function venugopalaswamy() {
+    // Display an alert
+    alert('Click on OK to see more about Sri Venugopala Swamy Temple');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'venugopalaswamy.html';
+}
